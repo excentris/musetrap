25d8680e88c22023cbcd86e9e0774f7b4dfe68e2
Eduardo Rubio <netrunner@gmail.com>
2015-07-01 20:18:15 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name musetrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the musetrapApp
+ */
+angular.module('musetrapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
