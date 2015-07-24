2b93b6c25da9ae002d7e3eb60b5d1125cb7e493b
Eduardo Rubio <netrunner@gmail.com>
2015-07-24 14:48:07 +0200

    Added unit test for getRecipe with mocked data service

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,26 +5,45 @@ describe('Controller: MainCtrl', function() {
   // load the controller's module
   beforeEach(module('musetrapApp'));
 
-  var MainCtrl,
-    $scope;
+  var MainCtrl, $scope, $rootScope, $q, queryDeferred;
+  var expectedResponse = [{
+    "name": "Sword"
+  }, {
+    "name": "Axe"
+  }, {
+    "name": "Crossbow"
+  }, {
+    "name": "Pistol"
+  }];
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($controller, $rootScope) {
-    // TODO create a mocked Data service and inject it into the controller
+  beforeEach(inject(function($controller, _$q_, _$rootScope_) {
     var mockedDataService = {
       getIngredients: function() {
-        // return a promise
+        queryDeferred = $q.defer();
+        return queryDeferred.promise;
       }
     };
 
+    $q = _$q_;
+    $rootScope = _$rootScope_;
     $scope = $rootScope.$new();
+
     MainCtrl = $controller('MainCtrl', {
       $scope: $scope,
       Data: mockedDataService
-    });
-  }));
+    });}));
 
   it('should initially have no ingredients', function() {
     expect($scope.ingredients.length).toBe(0);
+  });
+
+  it('should initially have 4 ingredients', function() {
+    $scope.getRecipe();
+    // resolve the deferred with a response. 'then' is not yet called.
+    queryDeferred.resolve(expectedResponse);
+    // propagate promise resolution to 'then'
+    $rootScope.$apply();
+    expect($scope.ingredients.length).toBe(4);
   });
 });
