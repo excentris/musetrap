102df39c91baacdcc6f592b437472f0786493ff0
Eduardo Rubio <netrunner@gmail.com>
2015-11-16 19:38:22 +0100

    Correctly set active classes for navigation and language buttons

--- app/scripts/controllers/footer.js
+++ app/scripts/controllers/footer.js
@@ -8,14 +8,32 @@
  * Controller of the musetrapApp
  */
 angular.module('musetrapApp')
-  .controller('FooterCtrl', ['$scope', '$translate',
-    function($scope, $translate) {
+  .controller('FooterCtrl', ['$scope', '$translate', '$location',
+    function($scope, $translate, $location) {
       /**
        * Change the application language.
        * @param langKey e.g. 'en', 'es'
        */
       $scope.changeLanguage = function(langKey) {
         $translate.use(langKey);
+      };
+
+      /**
+       * Check if the specified langKey corresponds to the active language.
+       * @param  String langKey the key of the language to check if it is active.
+       * @return true if the specified langKey corresponds to the active language.
+       */
+      $scope.isActiveLanguage = function(langKey) {
+        return langKey === $translate.use();
+      };
+
+      /**
+       * Check if the specified location is active.
+       * @param  String viewLocation the location check
+       * @return true if the specified location is active, false otherwise
+       */
+      $scope.isActiveLocation = function(viewLocation) {
+        return viewLocation === $location.path();
       };
     }
   ]);
