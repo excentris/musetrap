c9c693cb1ee755a7ae1d409b230039728ec70a67
Eduardo Rubio <netrunner@gmail.com>
2015-12-02 21:19:45 +0100

    Added some Data factory tests
    Fixes #11

--- test/spec/services/data.js
+++ test/spec/services/data.js
@@ -6,12 +6,36 @@ describe('Service: Data', function() {
   beforeEach(module('musetrapApp'));
 
   // instantiate service
-  var Data;
-  beforeEach(inject(function(_Data_) {
+  var Data, $httpBackend, NotificationFactory;
+  beforeEach(inject(function(_Data_, _$httpBackend_, _NotificationFactory_) {
+    $httpBackend = _$httpBackend_;
     Data = _Data_;
+    NotificationFactory = _NotificationFactory_;
+
+    // swallow i18n calls
+    $httpBackend.when('GET', /i18n/).respond({});
+
+    spyOn(NotificationFactory, 'error');
   }));
 
-  it('should do something', function() {
-    expect(!!Data).toBe(true);
+  it('should display an error when getAvailableRecipes fails', function() {
+    $httpBackend.when('GET', 'data/recipes.json').respond(401, '');
+    Data.getAvailableRecipes();
+    $httpBackend.flush();
+    expect(NotificationFactory.error).toHaveBeenCalled();
+  });
+
+  it('should display an error when getAvailableBundles fails', function() {
+    $httpBackend.when('GET', 'metadata.json').respond(401, '');
+    Data.getAvailableBundles();
+    $httpBackend.flush();
+    expect(NotificationFactory.error).toHaveBeenCalled();
+  });
+
+  it('should display an error when getAvailableBundles fails', function() {
+    $httpBackend.when('GET', 'metadata.json').respond(401, '');
+    Data.getAvailableLanguages();
+    $httpBackend.flush();
+    expect(NotificationFactory.error).toHaveBeenCalled();
   });
 });
