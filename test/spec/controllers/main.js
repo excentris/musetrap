3a3124b4159ee515e08844014e2b6f504d9bd768
Eduardo Rubio <netrunner@gmail.com>
2015-12-08 22:14:16 +0100

    Added test to check a notification is displayed when getIngredients fails

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,22 +5,12 @@ describe('Controller: MainCtrl', function() {
   // load the controller's module
   beforeEach(module('musetrapApp'));
 
-  var MainCtrl, $scope, $rootScope, $q, queryDeferred, $httpBackend;
-  var expectedResponse = [{
-    "data": [{
-      "name": "Sword"
-    }, {
-      "name": "Axe"
-    }, {
-      "name": "Crossbow"
-    }, {
-      "name": "Pistol"
-    }]
-  }];
+  var MainCtrl, $scope, $rootScope, $q, queryDeferred, $httpBackend, NotificationFactory;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function(_$httpBackend_, $controller, _$q_, _$rootScope_) {
+  beforeEach(inject(function(_$httpBackend_, $controller, _$q_, _$rootScope_, _NotificationFactory_) {
     $httpBackend = _$httpBackend_;
+    NotificationFactory = _NotificationFactory_;
 
     // swallow i18n calls
     $httpBackend.when('GET', /i18n/).respond({});
@@ -36,6 +26,8 @@ describe('Controller: MainCtrl', function() {
     $rootScope = _$rootScope_;
     $scope = $rootScope.$new();
 
+    spyOn(NotificationFactory, 'error');
+
     MainCtrl = $controller('MainCtrl', {
       $scope: $scope,
       Data: mockedDataService
@@ -48,10 +40,30 @@ describe('Controller: MainCtrl', function() {
 
   it('should have 1 ingredient after sampling the response on createRecipe', function() {
     $scope.createRecipe();
+    var expectedResponse = [{
+      "data": [{
+        "name": "Sword"
+      }, {
+        "name": "Axe"
+      }, {
+        "name": "Crossbow"
+      }, {
+        "name": "Pistol"
+      }]
+    }];
     // resolve the deferred with a response. 'then' is not yet called.
     queryDeferred.resolve(expectedResponse);
     // propagate promise resolution to 'then'
     $rootScope.$apply();
     expect($scope.recipeResults.length).toBe(1);
+  });
+
+  it('should display an error when getIngredients fails when createRecipe is called', function() {
+    $scope.createRecipe();
+    // resolve the deferred with a response. 'then' is not yet called.
+    queryDeferred.reject();
+    // propagate promise resolution to 'then'
+    $rootScope.$apply();
+    expect(NotificationFactory.error).toHaveBeenCalled();
   });
 });
