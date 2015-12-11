428e30a14adbbca67bfdb310895881fefa0263f0
Eduardo Rubio <netrunner@gmail.com>
2015-12-11 16:03:25 +0100

    Refactored bundle creation target

--- grunt/tasks/newBundle.js
+++ grunt/tasks/newBundle.js
@@ -7,32 +7,37 @@ module.exports = function(grunt) {
     }
 
     var done = this.async();
+    var locales = grunt.config.get('i18n.locales');
     var ingredientsArray = [];
-
-    // TODO build questions dinamically to add questions for translations
-    var questions = [{
-      type: "input",
-      name: "ingredient",
-      message: "Ingredient key: "
-    }, {
-      type: "confirm",
-      name: "askAgain",
-      message: "Another ingredient?",
-      default: true
-    }];
+    var questions = buildQuestions();
+    var translations = {};
+    // initialize translation objects, one per locale
+    locales.forEach(function(locale) {
+      translations[locale] = {};
+    });
 
     function ask() {
       inquirer.prompt(questions, function(answers) {
-        ingredientsArray.push(answers.ingredient);
+        if (answers.ingredient) {
+          ingredientsArray.push(answers.ingredient);
+        }
+
+        locales.forEach(function(locale) {
+          if (answers[locale]) {
+            translations[locale][answers.ingredient] = answers[locale];
+          }
+        });
+
         if (answers.askAgain) {
           ask();
         }
         else {
           createBundleFile(bundleName, ingredientsArray);
+          createTranslationFiles(bundleName, translations);
           done();
         }
       });
-    }
+    };
 
     function createBundleFile(bundleName, ingredientsArray) {
       // write bundle
@@ -42,7 +47,49 @@ module.exports = function(grunt) {
         grunt.file.write(bundleFile, JSON.stringify(ingredientsArray, null, '\t'));
       }
       grunt.task.run(['sortBundle:' + bundleName]);
-      grunt.task.run(['createTranslationFiles:' + bundleName]);
+    };
+
+    function createTranslationFiles(bundleName, translations) {
+      // write translation file
+      var locales = grunt.config.get('i18n.locales');
+      var destPath = grunt.config.get('data.ingredient_bundles.dest');
+
+      for (var locale in translations) {
+        if (translations.hasOwnProperty(locale)) {
+          var i18nDestPath = grunt.config.get('i18n.ingredient_bundles.dest');
+          var bundleTranslationSkeletonPath = i18nDestPath + bundleName + "_" + locale + ".json";
+          if (!grunt.file.exists(bundleTranslationSkeletonPath)) {
+            grunt.file.write(bundleTranslationSkeletonPath, JSON.stringify(translations[locale], null, '\t'));
+          }
+        }
+      }
+    };
+
+    function buildQuestions() {
+      var questions = [];
+
+      questions.push({
+        type: "input",
+        name: "ingredient",
+        message: "Ingredient key: "
+      });
+
+      locales.forEach(function(locale) {
+        questions.push({
+          type: "input",
+          name: locale,
+          message: "Translation (" + locale + "):"
+        });
+      });
+
+      questions.push({
+        type: "confirm",
+        name: "askAgain",
+        message: "Another ingredient?",
+        default: true
+      });
+
+      return questions;
     };
 
     ask();
