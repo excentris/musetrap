7d674c96dc41288d6c03104e4186a70a20d5a5e6
Eduardo Rubio <netrunner@gmail.com>
2015-12-14 21:05:15 +0100

    Initial drag-drop implementation

--- test/spec/controllers/configuration.js
+++ test/spec/controllers/configuration.js
@@ -86,36 +86,12 @@ describe('Controller: ConfigurationCtrl', function() {
     expect($scope.availableBundles.length).toBe(6);
   });
 
-  it('should have 1 selected module after selecting 1 module with an initially empty selection', function() {
-    $scope.availableBundles = ['animals', 'weapons'];
-    $scope.selectedBundles = [];
-    // toggle selection. Only 'animals' module selected
-    $scope.toggleBundleSelection('animals');
-    expect($scope.selectedBundles).toEqual(['animals']);
-    expect($scope.availableBundles.length).toBe(2);
-
+  it('should correctly register translation parts for selected bundles', function() {
+    $scope.selectedBundles = ['animals'];
     // apply changes for the $scope.$watchCollection to be triggered
     $scope.$apply();
     // the animals part should have been added to the registered parts
     expect($translatePartialLoader.getRegisteredParts()).toEqual(['app/ui', 'recipes', 'ingredient_bundles', 'ingredient_bundles/animals']);
-  });
-
-  it('should have 0 selected modules after deselecting the only one selected', function() {
-    $scope.availableBundles = ['animals', 'weapons'];
-    $scope.selectedBundles = ['weapons'];
-    // toggle selection. Deselect 'weapons'
-    $scope.toggleBundleSelection('weapons');
-    expect($scope.selectedBundles.length).toBe(0);
-    expect($scope.availableBundles.length).toBe(2);
-  });
-
-  it('should not be possible to select a non available module', function() {
-    $scope.availableBundles = ['animals', 'weapons'];
-    $scope.selectedBundles = [];
-    // toggle selection with a bogus module
-    $scope.toggleBundleSelection('bogus');
-    expect($scope.selectedBundles.length).toBe(0);
-    expect($scope.availableBundles.length).toBe(2);
   });
 
   it('should correctly toggle the mode', function() {
